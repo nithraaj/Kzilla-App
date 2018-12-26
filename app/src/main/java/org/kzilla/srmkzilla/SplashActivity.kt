1c0c4179cc3d6c8c6749da91b346e3d17406c5c4
Nithesh P <nithraaj@gmail.com>
2018-12-27 04:45:37 +0530

    Fix firestore timestamp warning

--- app/src/main/java/org/kzilla/srmkzilla/SplashActivity.kt
+++ app/src/main/java/org/kzilla/srmkzilla/SplashActivity.kt
@@ -14,6 +14,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.FirebaseFirestoreSettings
 import kotlinx.android.synthetic.main.activity_splash.*
 import java.io.IOException
 import java.net.InetSocketAddress
@@ -126,6 +127,10 @@ class SplashActivity : AppCompatActivity() {
 
     private fun checkUserData(online: Boolean) {
         val db = FirebaseFirestore.getInstance()
+        val settings = FirebaseFirestoreSettings.Builder()
+            .setTimestampsInSnapshotsEnabled(true)
+            .build()
+        db.setFirestoreSettings(settings)
         /*
         FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
                 .setPersistenceEnabled(false)
