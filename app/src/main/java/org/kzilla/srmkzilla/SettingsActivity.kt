5c1c9fd2b998104e4195def300ed8614a77f0edf
Nithesh P <nithraaj@gmail.com>
2018-12-28 02:26:30 +0530

    Added toolbar menu

--- app/src/main/java/org/kzilla/srmkzilla/SettingsActivity.kt
+++ app/src/main/java/org/kzilla/srmkzilla/SettingsActivity.kt
@@ -1,12 +1,18 @@
 package org.kzilla.srmkzilla
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.preference.PreferenceManager
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_settings.*
 
 class SettingsActivity : AppCompatActivity() {
 
+    lateinit var context: Context
     override fun onCreate(savedInstanceState: Bundle?) {
         val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
         val theme: Int
@@ -18,7 +24,7 @@ class SettingsActivity : AppCompatActivity() {
         setTheme(theme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
-        val context = this
+        context = this
 
         val editor = sharedPreferences.edit()
         if (sharedPreferences.getString("theme", "light") == "dark") {
@@ -34,5 +40,31 @@ class SettingsActivity : AppCompatActivity() {
             }
             this@SettingsActivity.recreate()
         }
+    }
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.toolbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val id = item.itemId
+
+
+        if (id == R.id.settings) {
+            val settingIntent = Intent(context, SettingsActivity::class.java)
+            startActivity(settingIntent)
+        } else if (id == R.id.logout) {
+            val mAuth = FirebaseAuth.getInstance()
+            mAuth.signOut()
+            val intent = Intent(context, AuthActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+            finish()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
