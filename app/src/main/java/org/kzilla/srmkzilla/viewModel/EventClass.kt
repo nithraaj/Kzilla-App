b38adb61462532ded884a99c4bd82f3717857d2f
Nithesh P <nithraaj@gmail.com>
2019-01-03 00:51:35 +0530

    Added viewmodel for events fragment

---
+++ app/src/main/java/org/kzilla/srmkzilla/viewModel/EventClass.kt
@@ -0,0 +1,39 @@
+package org.kzilla.srmkzilla.viewModel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import org.kzilla.srmkzilla.db.CarouselRepository
+import org.kzilla.srmkzilla.db.RegisteredEventsRepository
+import org.kzilla.srmkzilla.db.UpcomingEventsRepository
+
+class EventClass(application: Application):AndroidViewModel(application) {
+
+    lateinit var _upcomingEvents:LiveData<UpcomingEventsRepository.UpcomingEventsData>
+    lateinit var _registeredEvents:LiveData<RegisteredEventsRepository.RegisteredEventsData>
+    lateinit var _carousel:LiveData<CarouselRepository.Carousel>
+
+    private var upcomingEventsRepo:UpcomingEventsRepository = UpcomingEventsRepository.init(application)
+    private var registeredEventsRepo:RegisteredEventsRepository = RegisteredEventsRepository.init(application)
+    private var carouselRepo:CarouselRepository = CarouselRepository.init(application)
+
+
+
+    fun getUpcomingEvents(fromNetwork:Boolean?):LiveData<UpcomingEventsRepository.UpcomingEventsData> {
+        _upcomingEvents = upcomingEventsRepo.getEvents(fromNetwork)
+        return _upcomingEvents
+    }
+
+    fun getRegisteredEvents(fromNetwork:Boolean?):LiveData<RegisteredEventsRepository.RegisteredEventsData> {
+        _registeredEvents = registeredEventsRepo.getRegistrations(fromNetwork)
+        return _registeredEvents
+    }
+
+    fun getCarousel(fromNetwork: Boolean?):LiveData<CarouselRepository.Carousel> {
+        _carousel = carouselRepo.getCarousels(fromNetwork)
+        return _carousel
+    }
+
+
+}
+
